d7aafea4ad1b755dc33938e5ca6a25552a477a93
MicrowavedBunny <jayz34567@gmail.com>
2021-08-15 12:34:08 -0400

    added pdf displayer

---
+++ src/Resume.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import resumePDF from "./Resume-Jacob-Davidson.pdf";
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
+class Resume extends Component {
+    render() {
+        return (
+            <div style={{display: "flex"}}>
+                <Document file={resumePDF}>
+                <Page pageNumber={1}/>
+                </Document>
+                <Document file={resumePDF}>
+                    <Page pageNumber={2}/>
+                </Document>
+            </div>
+        );
+    }
+}
+
+export default Resume;
--- src/Stuff.js
+++ src/Stuff.js
@@ -7,9 +7,9 @@ class Stuff extends Component {
                 <h2>Portfolio</h2>
                 <p>Here is an example of a GitHub Page I have published:</p>
                 <iframe title="WeatherApp" src="https://microwavedbunny.github.io/WeatherApp/" width="100%" height="400" frameBorder="0"
-                        scrolling="no"></iframe>
-                <p>THis is a link to my Github Page!</p>
-                <a href="https://github.com/MicrowavedBunny" ><img src={gh} alt="GitHub"/></a>
+                        scrolling="no"/>
+                <p>This is a link to my Github Page!</p>
+                <a href="https://github.com/MicrowavedBunny"><img src={gh} alt="GitHub"/></a>
             </div>
         );
     }
